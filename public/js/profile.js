eaf47c314b659c83b889b14e1d74a39db03cd7d8
German27 <german.velasco98@gmail.com>
2019-01-04 13:25:16 -0300

    Profile buttons & validation

---
+++ public/js/profile.js
@@ -0,0 +1,134 @@
+window.onload = function () {
+
+  var regexEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  var formProfile = document.getElementById("registrationForm");
+  var elements = formProfile.elements;
+  elements = Array.from(elements);
+  elements.pop();
+  elements.pop();
+
+  var editButton = document.getElementById("editButton");
+  editButton.addEventListener('click', enableEdition);
+
+  var discardButton = document.getElementById("discardButton");
+  discardButton.addEventListener('click', discardEdition);
+
+  function enableEdition() {
+    elements.forEach(function (element) {
+      element.removeAttribute("disabled");
+    });
+
+    inputEmail = document.querySelector('input[name="email"]');
+    inputEmail.setAttribute("disabled", "disabled");
+
+    inputFile = document.querySelector(".input-file-ale");
+    inputFile.removeAttribute("disabled");
+
+    editButton = document.getElementById("editButton");
+    editButton.disabled = "disabled";
+
+    discardButton = document.getElementById("discardButton");
+    discardButton.removeAttribute("hidden");
+
+    saveButton = document.getElementById("saveButton");
+    saveButton.removeAttribute("hidden");
+  }
+
+  function discardEdition() {
+    elements.forEach(function (element) {
+      element.setAttribute("disabled", "disabled");
+      element.classList.remove("is-invalid");
+      element.classList.remove("is-valid");
+    });
+
+    inputFile = document.querySelector(".input-file-ale");
+    inputFile.setAttribute("disabled", "disabled");
+    inputFile.classList.remove("is-invalid");
+    inputFile.classList.remove("is-valid");
+
+    editButton = document.getElementById("editButton");
+    editButton.removeAttribute("disabled");
+
+    discardButton = document.getElementById("discardButton");
+    discardButton.setAttribute("hidden", "hidden");
+
+    saveButton = document.getElementById("saveButton");
+    saveButton.setAttribute("hidden", "hidden");
+
+  }
+
+  formProfile.addEventListener('submit', function (event) {
+
+    if (
+      inputName.value.trim() === "" || inputName.value.length < 4 || inputName.value.length > 30 ||
+      inputLastname.value.trim() === "" || inputLastname.value.length < 4 || inputLastname.value.length > 30 ||
+      inputEmail.value.trim() === "" || !regexEmail.test((inputEmail.value))
+    ) {
+      event.preventDefault();
+      elements.forEach(function (element) {
+        if (element.value == "") {
+          element.classList.add("is-invalid");
+          var div = element.parentElement.querySelector('.invalid-feedback');
+          console.log(div);
+          div.innerHTML = 'Campo obligatorio';
+        }
+      });
+      console.log(elements);
+      // alert("Error en algun campo");
+    }
+  });
+
+  var inputName = document.querySelector('input[name="name"]');
+  var inputLastname = document.querySelector('input[name="lastname"]');
+  var inputPais = document.querySelector('input[name="country"]');
+
+  //Name validation
+  inputName.addEventListener('keyup', function (event) {
+    if ((this.value).length < 4) {
+      this.classList.add('is-invalid');
+      var div = this.parentElement.querySelector('.invalid-feedback');
+      div.innerHTML = 'El nombre es demasiado corto';
+    } else if ((this.value).length > 30) {
+      this.classList.add('is-invalid');
+      var div = this.parentElement.querySelector('.invalid-feedback');
+      div.innerHTML = 'El nombre es demasiado largo';
+    }
+    else {
+      this.classList.remove("is-invalid");
+      this.classList.add('is-valid');
+    }
+  });
+
+  //Lastname validation
+  inputLastname.addEventListener('keyup', function (event) {
+    if ((this.value).length < 4) {
+      this.classList.add('is-invalid');
+      var div = this.parentElement.querySelector('.invalid-feedback');
+      div.innerHTML = 'El apellido es demasiado corto';
+    }
+    else if ((this.value).length > 30) {
+      this.classList.add('is-invalid');
+      var div = this.parentElement.querySelector('.invalid-feedback');
+      div.innerHTML = 'El apellido es demasiado largo';
+    }
+    else {
+      this.classList.remove("is-invalid");
+      this.classList.add('is-valid');
+    }
+  });
+
+  //Email validation
+  // inputEmail.addEventListener('keyup', function (event) {
+  //   if (!regexEmail.test((this.value))) {
+  //     this.classList.add('is-invalid');
+  //     var div = this.parentElement.querySelector('.invalid-feedback');
+  //     div.innerHTML = 'El formato de email es invalido';
+  //   }
+  //   else {
+  //     this.classList.remove("is-invalid");
+  //     this.classList.add('is-valid');
+  //   }
+  // });
+
+}
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -19,95 +19,73 @@
       </div>
 
       <div class="col-sm-9">
-        <br>
-        <br>
-      <form class="form perfil-form clearfix" action="" method="post" id="registrationForm" enctype="multipart/form-data">
-          @csrf
-	        	{{ method_field('PUT') }}
-       
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col">
-                <label><h6>Nombre: </h6></label>
-              <input type="text" class="form-control" name="name" value="" disabled>
-                <div class="invalid-feedback">
+        <button class="btn btn-info float-right" type="button" id="editButton">Editar perfil</button>
+        <form class="form profile-form clearfix mt-5" action="" method="post" id="registrationForm" enctype="multipart/form-data">
+            @csrf
+              {{ method_field('PUT') }}
+        
+            <div class="form-group">
+              <div class="form-row">
+                <div class="col">
+                  <label><h6>Nombre: </h6></label>
+                <input type="text" class="form-control" name="name" value="" disabled>
+                  <div class="invalid-feedback">
+                  </div>
+                </div>
+                <div class="col">
+                  <label><h6>Apellido:</h6></label>
+                  <input type="text" class="form-control" name="lastname" value="" disabled>
+                  <div class="invalid-feedback">
+                  </div>
                 </div>
               </div>
-              <div class="col">
-                <label><h6>Apellido:</h6></label>
-                <input type="text" class="form-control" name="lastname" value="" disabled>
+            </div>
+
+            <div class="form-group">
+              <div class="form-row">
+                {{-- <div class="col">
+                  <label><h6>Nombre de Usuario:</h6></label>
+                  <input type="text" class="form-control" name="username" value="" disabled>
+                  <div class="invalid-feedback">
+                  </div>
+                </div> --}}
+                <div class="col">
+                  <label><h6>Pais:</h6></label>
+                  <select id="inputState" class="form-control" name="country" value="" disabled>
+                    
+                  </select>
+                  <div class="invalid-feedback">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-xs-6">
+                <label><h6>Email:</h6></label>
+                <input type="email" class="form-control" name="email" value="" disabled>
                 <div class="invalid-feedback">
                 </div>
               </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <div class="form-row">
-              {{-- <div class="col">
-                <label><h6>Nombre de Usuario:</h6></label>
-                <input type="text" class="form-control" name="username" value="" disabled>
-                <div class="invalid-feedback">
-                </div>
-              </div> --}}
-              <div class="col">
-                <label><h6>Pais:</h6></label>
-                <select id="inputState" class="form-control" name="country" value="" disabled>
-                  
-                </select>
-                <div class="invalid-feedback">
-                </div>
+            <h6>Subí tu foto...</h6>
+            <div class="custom-file form-group">
+              <input type="file" class="custom-file-input text-center center-block input-file-ale" id="customFile" name="userPhoto" disabled>
+              <label class="custom-file-label text-left" for="customFile">Elegir</label>
+              <span></span>
+            </div>
+
+            <div class="form-group">
+              <div class="col-xs-12">
+                <button class="btn btn-danger float-left" type="button" id="discardButton" hidden>Descartar</button>
+                <button class="btn btn-success float-right" type="submit" id="saveButton" hidden>Guardar cambios</button>
               </div>
             </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-6">
-              <label><h6>Email:</h6></label>
-              <input type="email" class="form-control" name="email" value="" disabled>
-              <div class="invalid-feedback">
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col">
-                <label><h6>Contraseña:</h6></label>
-                <input type="password" class="form-control" name="password" value="" disabled>
-                <div class="invalid-feedback">
-                </div>
-              </div>
-              <div class="col">
-                <label><h6>Confirmar contraseña:</h6></label>
-                <input type="password" class="form-control" name="passwordR" value="" disabled>
-                <div class="invalid-feedback">
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <h6>Subí tu foto...</h6>
-          <div class="custom-file form-group">
-            <input type="file" class="custom-file-input text-center center-block input-file-ale" id="customFile" name="userPhoto" disabled>
-            <label class="custom-file-label text-left" for="customFile">Elegir</label>
-            <span></span>
-          </div>
-
-          <div class="form-group">
-            <div class="col-xs-12">
-              <br>
-              <button class="btn btn-danger float-left" type="reset" id="" hidden>Descartar</button>
-              <button class="btn btn-success float-right" type="submit" id="" hidden>Guardar cambios</button>
-            </div>
-          </div>
-        </form>
-
+          </form>
 
       </div>
     </div>
   </div>
-
 
 @endsection
