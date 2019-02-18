fcb0d9a8ff3ba6013cfacb757b9ded913bdcc122
German27 <german.velasco98@gmail.com>
2019-02-18 18:44:13 -0300

    Changes in cart view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -18,7 +18,7 @@
       <p class="">ProductQuantity</p>
     </div>
     <div class="float-sm-right">
-      <p class="font-weight-bold text-success mr-2 ml-4">$0.00</p>
+      <p class="font-weight-bold text-success mr-2 ml-4 d-block">$0.00</p>
     </div>  
     <div class="d-block p-3">
        <button class="btn btn-secondary mr-2">Editar</button>
@@ -27,17 +27,25 @@
   </div>
   <div class="col-12 col-lg-4">
     <div class="bg-dark mt-4 pb-1 rounded">
-      <div class="bg-danger p-1 rounded border border-secondary border-top-0 border-right-0 border-left-0">
+      <div class="bg-danger p-1 rounded border border-secondary border-top-0 border-right-0 border-left-0 mb-3">
         <h2 class="text-center text-light">Resumen</h2>
       </div>
-      <p class="text-light m-3">Subtotal: $0.00</p>
-      <p class="text-light m-3">Coste de envio: $0.00</p>
-      <p class="text-light m-3 font-weight-bold">Total: $0.00</p>
-      <button class="col-8 offset-2 btn btn-success btn-block text-center mt-4">Pagar</button>
-      <p class="text-light font-weight-bold text-center m-2">O</p>
-      <button class="col-8 offset-2 btn btn-secondary btn-block text-center">Seguir comprando</button>
-      <p class="text-light ml-3 mt-5 mb-0 font-weight-bold">Atencion!</p>
-      <p class="text-light ml-3">Aunque tus productos esten en tu carrito de compras, pueden agotarse.</p>
+      <div class="col-12">
+        <p class="text-light">Subtotal: $0.00</p>
+        <p class="text-light">Coste de envio: $0.00</p>
+        <p class="text-light font-weight-bold">Total: $0.00</p>
+      </div>
+      <div class="col-12">
+        <div class="m-4">
+          <button class="btn btn-success btn-block font-weight-bold">PAGAR</button>
+          <p class="text-light font-weight-bold text-center m-2">O</p>
+          <button class="btn btn-secondary btn-block font-weight-bold">SEGUIR COMPRANDO</button>
+        </div>
+      </div>
+      <div class="col-12">
+        <p class="text-light font-weight-bold mb-0">Date prisa!</p>
+        <p class="text-light">Aunque tus productos esten en tu carrito de compras, pueden agotarse.</p>
+      </div>
     </div>    
   </div>
 
