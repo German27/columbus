8ced99e582c190ea332d22eeae1c2608930621ea
German27 <german.velasco98@gmail.com>
2019-02-19 17:08:31 -0300

    Footer corrections

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,67 +1,32 @@
 <footer>
-
-    <div class="container text-center text-md-left mt-5">
-    <hr>
-    <br>
-      <div class="row mt-3 dark-grey-text">
-
+  <div class="container">
+    <div class="text-center text-md-left mt-5">
+      <div class="row mt-3">
         <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
-
           <h6 class="text-uppercase font-weight-bold">Columbus</h6>
-          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>Somos una empresa dedicada a la comercialización de indumentaria,<br>a través de Internet, con la posibilidad de ir ampliando progresivamente.</p>
-
+          <p>Somos una empresa dedicada a la comercialización de indumentaria, a través de Internet, con la posibilidad de ir ampliando progresivamente.</p>
         </div>
-
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
           <h6 class="text-uppercase font-weight-bold">Atención al Cliente</h6>
-          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>
-            <a class="dark-grey-text" href="/contact" >Contacto</a>
-          </p>
-          <p>
-            <a class="dark-grey-text" href="/faq">Preguntas Frecuentes</a>
-          </p>
-          <p>
-            <a class="dark-grey-text" href="/policy">Privacidad y terminos</a>
-          </p>
+          <a class="d-block" href="" >Contacto</a>
+          <a class="d-block" href="">Preguntas Frecuentes</a>
+          <a class="d-block" href="">Privacidad y terminos</a>
         </div>
-
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
           <h6 class="text-uppercase font-weight-bold">Usuario</h6>
-          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>
-            <a class="dark-grey-text" href="/profile">Mi cuenta</a>
-          </p>
-          <p>
-            <p class="dark-grey-text" href="#">Nuestros temas:</p>
-          </p>
-          <button class="btn btn-secondary" id="light"></button>
-          <button class="btn btn-light" id="relax"></button>
-
+          <a class="" href="">Mi cuenta</a>
         </div>
-
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
           <h6 class="text-uppercase font-weight-bold" id="contacto">Contacto</h6>
-          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-            <p class="fa fa-home mr-3">Lima 1111, CABA, AR</p>
-            <p class="fa fa-envelope mr-3">info@gmail.com</p>
-            <p class="fa fa-phone mr-3">+54 9 4443-7466</p>
-
+          <p class="fa fa-home mr-3">Lima 1111, CABA, AR</p>
+          <p class="fa fa-envelope mr-3">info@gmail.com</p>
+          <p class="fa fa-phone mr-3">+54 9 4443-7466</p>
         </div>
-
       </div>
-
     </div>
-
-    <div class="footer-copyright text-center text-black-50 py-3">© 2018 Copyright:
-      <a class="dark-grey-text" href="">Columbus.com</a>
-    </div>
-
-
-
-
-  </footer>
+    <div class="footer-copyright text-center py-3">
+      <span class="text-black-50">© 2018 Copyright:</span> 
+      <a class="" href="">Columbus.com</a>
+    </div>   
+  </div>
+</footer>
